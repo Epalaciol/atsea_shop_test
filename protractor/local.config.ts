5a093153f4389c8a6b2b3c7b1efd9920fe8a0fbe
Daniela <dhiguitaa17@gmail.com>
2021-03-09 09:58:28 -0500

    buy flow works in chrome and firefox

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -1,54 +1,42 @@
 import { browser, Config } from "protractor";
 
-// const firefoxConfig = {
-//   browserName: "firefox",
-//   name: "firefox-tests",
-//   shardTestFiles: true,
-//   maxInstances: 1,
-//   "moz:firefoxOptions": {
-//     args: [
-//       "--disable-popup-blocking",
-//       "--no-default-browser-check",
-//       "--window-size=800,600",
-//     ],
-//   },
-// };
+const firefoxConfig = {
+  browserName: "firefox",
+  name: "firefox-tests",
+  shardTestFiles: true,
+  maxInstances: 1,
+  "moz:firefoxOptions": {
+    args: [
+      "--disable-popup-blocking",
+      "--no-default-browser-check",
+      "--window-size=800,600",
+    ],
+  },
+};
 
-// const chromeConfig = {
-//   browserName: "chrome",
-//   name: "chrome-tests",
-//   shardTestFiles: true,
-//   maxInstances: 1,
-//   chromeOptions: {
-//     args: [
-//       "--disable-popup-blocking",
-//       "--no-default-browser-check",
-//       "--window-size=800,600",
-//     ],
-//     prefs: { credentials_enable_service: false },
-//   },
-// };
+const chromeConfig = {
+  browserName: "chrome",
+  name: "chrome-tests",
+  shardTestFiles: true,
+  maxInstances: 1,
+  chromeOptions: {
+    args: [
+      "--disable-popup-blocking",
+      "--no-default-browser-check",
+      "--window-size=800,600",
+    ],
+    prefs: { credentials_enable_service: false },
+  },
+};
 
-// const multiCapabilities = [chromeConfig];
+const multiCapabilities = [chromeConfig, firefoxConfig];
 
 export const config: Config = {
-  // multiCapabilities,
+  multiCapabilities,
   framework: "mocha",
   mochaOpts: {
     reporter: "mochawesome-screenshots",
     timeout: 120000,
-  },
-  capabilities: {
-    browserName: "chrome",
-    shardTestFiles: true,
-    chromeOptions: {
-      args: [
-        "--disable-popup-blocking",
-        "--no-default-browser-check",
-        "--window-size=800,600",
-      ],
-      prefs: { credentials_enable_service: false },
-    },
   },
   specs: ["../test/ui/**/*.js"],
   getPageTimeout: 30000,
